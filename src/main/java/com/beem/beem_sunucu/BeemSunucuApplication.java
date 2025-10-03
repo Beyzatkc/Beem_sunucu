f3fa939332d3dbada5aa62531b49c799fb6c4562
WhalEmo <emrullah64uygun@gmail.com>
2025-10-03 22:03:15 +0300

    User entity and UserController

--- src/main/java/com/beem/beem_sunucu/BeemSunucuApplication.java
+++ src/main/java/com/beem/beem_sunucu/BeemSunucuApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeemSunucuApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Servis çalıştı");
 		SpringApplication.run(BeemSunucuApplication.class, args);
 	}
 
---
+++ src/main/java/com/beem/beem_sunucu/User/UserRepository.java
@@ -0,0 +1,8 @@
+package com.beem.beem_sunucu.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
